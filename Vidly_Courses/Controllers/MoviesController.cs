432a4a82d3cf6eafd2052eb32bb513c2369789dc
Shahriar Alam <shahriar.alam007@gmail.com>
2019-06-04 03:26:44 +0600

    Section 3 Exercise 3

--- Vidly_Courses/Controllers/MoviesController.cs
+++ Vidly_Courses/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,10 +10,27 @@ namespace Vidly_Courses.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movies = _context.Movies.Include(m => m.Genre).SingleOrDefault(i => i.Id == id);
             return View(movies);
         }
 
@@ -20,15 +38,6 @@ namespace Vidly_Courses.Controllers
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year+"/"+month);
-        }
-
-        private List<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie{Id = 1, Name = "Shrek!"},
-                new Movie{Id = 2, Name = "Wall-e"}
-            };
         }
     }
 }
--- Vidly_Courses/Models/Movie.cs
+++ Vidly_Courses/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Razor;
 
 namespace Vidly_Courses.Models
 {
@@ -9,5 +10,10 @@ namespace Vidly_Courses.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int NumberInStock { get; set; }
+        public Genre Genre { get; set; }
+        public int GenreId { get; set; }
     }
 }
