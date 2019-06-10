a7ac2b5e74b4e57d8bdd4e91a0ad46767fa833a5
Shahriar Alam <shahriar.alam007@gmail.com>
2019-06-10 23:20:19 +0600

    Section 4 before exercise

--- Vidly_Courses/Controllers/CustomersController.cs
+++ Vidly_Courses/Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly_Courses.Models;
+using Vidly_Courses.ViewModels;
 
 namespace Vidly_Courses.Controllers
 {
@@ -22,6 +23,37 @@ namespace Vidly_Courses.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm",viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(CustomerFormViewModel viewModel)
+        {
+            if (viewModel.Customer.Id == 0)
+            {
+                _context.Customers.Add(viewModel.Customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == viewModel.Customer.Id);
+
+                customerInDb.Name = viewModel.Customer.Name;
+                customerInDb.Birthday = viewModel.Customer.Birthday;
+                customerInDb.MembershipTypeId = viewModel.Customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = viewModel.Customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+            
+
+            return RedirectToAction("Index", "Customers");
+
+        }
         // GET: Customers
         public ActionResult Index()
         {
@@ -35,5 +67,21 @@ namespace Vidly_Courses.Controllers
             return View(customer);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
     }
 }
--- Vidly_Courses/Models/Customer.cs
+++ Vidly_Courses/Models/Customer.cs
@@ -12,9 +12,12 @@ namespace Vidly_Courses.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
         public DateTime ?Birthday { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
     }
 }
